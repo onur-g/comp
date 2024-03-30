a8997f4c3c1590ea1410cebb5ec8a11bf77c989f
Onur Gunduz <onurboragunduz@gmail.com>
2024-03-30 13:44:21 -0400

    263A-BeautifulMatrix

---
+++ codeforces/0200-0299/263A-BeautifulMatrix.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+
+#define DIM 5
+
+int main() {
+	int in = 0, cnt = -1;
+	
+	while (!in) {
+		scanf("%d", &in);
+		cnt++;
+	}
+	
+	int row = cnt / DIM, col = cnt % DIM;
+	
+	printf("%d", abs(row - 2) + abs(col - 2));
+	
+	return 0;
+}
