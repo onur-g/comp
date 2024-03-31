2be2d29ffa97d82f50a38bd11238a9103409c718
Onur Gunduz <onurboragunduz@gmail.com>
2024-03-31 19:16:57 -0400

    266B-QueueAtTheSchool

---
+++ codeforces/0200-0299/266B-QueueAtTheSchool.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,t;
+	string s;
+	cin>>n>>t>>s;
+	while(t--) {
+		for(int i=0;i<n-1;++i){
+			if(s[i]=='B'&&s[i+1]=='G'){
+				s[i+1]='B';
+				s[i]='G';
+				++i;
+			}
+		}
+	}
+	cout<<s;
+	return 0;
+}
