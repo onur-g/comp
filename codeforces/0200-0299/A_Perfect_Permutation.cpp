1fb60db98316a189f54edefbd7a9754ed897c77e
Onur Gunduz <onurboragunduz@gmail.com>
2024-04-21 14:48:31 -0400

    LeetCode 217. Contains Duplicate

---
+++ codeforces/0200-0299/A_Perfect_Permutation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool found = false;
+
+    for (int i = 0; i < n; ++i) {
+        if () {
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << -1;
+    }
+
+    cout << endl;
+}
